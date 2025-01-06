7912ca02d0d674a849b32788c0da576bd37b19f9
nuhmansha <nuhmanibnunazar@gmail.com>
2025-01-06 22:35:46 +0530

    mongodb and frontend connection completed

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
+const express = require("express");
+
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const dbConnection = require("./config/db");
+const questionRouter = require("./Router/question");
+
+const app = express();
+const port = process.env.PORT || 3002;
+
+// Middleware to parse JSON and URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/", questionRouter);
+
+dbConnection()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database", err);
+  });
 
